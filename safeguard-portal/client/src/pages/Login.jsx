7f844f81ec64efa02819454ee29c7699d9a89ff6
gurvirc <gca83@sfu.ca>
2026-03-18 18:03:16 -0700

    styled login form

--- safeguard-portal/client/src/pages/Login.jsx
+++ safeguard-portal/client/src/pages/Login.jsx
@@ -1,18 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import './Login.css'
 
 export default function Login() {
   const navigate = useNavigate();
 
   return (
-    <div style={{ padding: 24 }}>
-      <h2>Login</h2>
-      <p>Select a role (fake login for now):</p>
-
-      <div style={{ display: "flex", gap: 12 }}>
-        <button onClick={() => navigate("/client/dashboard")}>Login as Client</button>
-        <button onClick={() => navigate("/tech/dashboard")}>Login as Technician</button>
-        <button onClick={() => navigate("/admin/dashboard")}>Login as Admin</button>
+      <div className="login-page" style={{ display: "flex", gap: 12 }}>
+        <form className= "login-form">
+          <h1>Sign In</h1>
+          <p>Access your security dashboard</p>
+          <div className="input-box">
+            <h4>EMAIL ADDRESS</h4>
+            <input type="text" name="username" placeholder="Username" />
+          </div>
+          <div className="input-box">
+            <h4>PASSWORD</h4>
+            <input type="password" name="password" placeholder="Password"/>
+          </div>
+          <button className="login-btn">ACCESS DASHBOARD</button>
+        </form>
       </div>
-    </div>
-  );
+ 
+  )
 }
