875986bfec6ea93db60824179e667dc3ced8745e
gurvirc <gca83@sfu.ca>
2026-03-23 11:12:45 -0700

    added logout button

--- safeguard-portal/client/src/pages/admin/AdminDashboard.jsx
+++ safeguard-portal/client/src/pages/admin/AdminDashboard.jsx
@@ -1,11 +1,13 @@
-import Navbar from "../../components/Navbar";
+import AdminNavbar from "../../components/AdminNavbar.jsx";
+
 
 export default function AdminDashboard() {
   return (
     <div>
-          <Navbar />
+          
 
         <div style={{ padding: 24 }}>
+          <AdminNavbar />
             <h2>Admin Dashboard</h2>
             <p>Later: create clients/locations, create installations, assign techs.</p>
         </div>
